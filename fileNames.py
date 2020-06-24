2a48ee79e0e6aca07df5238f429219a00a5865a5
peng-soo <pengsoo22@gmail.com>
2020-06-24 18:08:48 +0900

    update 0624

--- fileNames.py
+++ fileNames.py
@@ -1,4 +1,5 @@
 from os import listdir
 from os.path import isfile, join
 onlyfiles = [f for f in listdir('./') if isfile(join('./', f))]
-print(onlyfiles)
+for fileName in onlyfiles :
+	print("<div onclick='onClick(this)'><img src='" + fileName + "'><span><br><input type='text' value='http://peng-soo.github.io/" + fileName + "'></span></div>")
